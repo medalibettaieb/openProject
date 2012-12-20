1f332539aa1d4bbdcae400e958b41e0a54f4453f
medalibettaieb <medali.bettaieb@esprit.ens.tn>
2012-12-20 12:21:13 +0100

    Signed-off-by: medalibettaieb <medali.bettaieb@esprit.ens.tn>

---
+++ edu.esprit.formationSocialCoding.smartRecipe/src/edu/esprit/formationSocialCoding/smartRecipe/tunisianFood/Plate.java
@@ -0,0 +1,19 @@
+package edu.esprit.formationSocialCoding.smartRecipe.tunisianFood;
+
+public class Plate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Plate plate = new Plate();
+		plate.cook();
+
+	}
+
+	public String cook() {
+		String slata = "";
+		return slata;
+	}
+
+}
